fca5ae938736e1b53ea3eee01a4c4677c035dd76
Chia-Chun Wu <ccwu0918@gmail.com>
2024-02-03 21:33:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ prompt=[
     """
     You are an expert in converting questions to SQL query!
     The SQL database has the name STUDENT and has the following columns - NAME, CLASS, 
-    SECTION \n\nFor example,\nExample 1 - How many entries of records are present?, 
+    SECTION, SCORE \n\nFor example,\nExample 1 - How many entries of records are present?, 
     the SQL command will be something like this SELECT COUNT(*) FROM STUDENT ;
     \nExample 2 - Tell me all the students studying in Data Science class?, 
     the SQL command will be something like this SELECT * FROM STUDENT 
